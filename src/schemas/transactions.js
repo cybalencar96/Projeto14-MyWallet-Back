cd6ebd655d45723d3937554eaa82e291a355ca15
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-10-23 19:57:53 -0300

    alteracao validacao valor e descricao

--- src/schemas/transactions.js
+++ src/schemas/transactions.js
@@ -1,8 +1,8 @@
 import joi from 'joi'
 
 const transactionSchema = joi.object({
-    value: joi.number().min(0.01).max(1000000).required(),
-    description: joi.string().min(3).max(100).required(),
+    value: joi.number().min(0.01).max(1000000000).required(),
+    description: joi.string().min(3).max(50).required(),
     entry: joi.boolean().required()
 })
 
