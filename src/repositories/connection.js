885ed16897efcf84baf94d6e9f0d2e4b92798d66
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-11-24 14:46:53 -0300

    add config deloy

--- src/repositories/connection.js
+++ src/repositories/connection.js
@@ -2,12 +2,24 @@ import pg from 'pg'
 
 const { Pool } = pg
 
-const connection = new Pool({
+let connectionData = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-});
+}
 
-export default connection
+if (process.env.NODE_ENV === 'prod') {
+    connectionData = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+}
+
+
+const connection = new Pool(connectionData);
+
+export default connection;
